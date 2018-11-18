57f7f17f00d42fbcbac611b0385c27e29cc54cc8
Miguel Campos <amodestduck@gmail.com>
2018-11-17 23:44:20 -0800

    preparing player prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour {
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

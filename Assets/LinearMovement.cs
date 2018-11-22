ab176f65a25d0d46127fb8c5d36754e7c0c5735b
Miguel Campos <amodestduck@gmail.com>
2018-11-21 19:51:57 -0800

    created linear movement script

---
+++ Assets/LinearMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class LinearMovement : MonoBehaviour {
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start () {
+        rb.velocity = new Vector2(0f, speed);
+    }
+
+}

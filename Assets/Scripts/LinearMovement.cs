98c82b4d8dbd6f3df0acebc917b64fbcb5148ef9
Miguel Campos <amodestduck@gmail.com>
2018-11-21 20:30:04 -0800

    created player movement script

--- Assets/Scripts/LinearMovement.cs
+++ Assets/Scripts/LinearMovement.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class LinearMovement : MonoBehaviour {
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float speed;
+    [SerializeField] private float linearSpeed;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Start () {
-        rb.velocity = new Vector2(0f, speed);
+ 
+    void FixedUpdate() {
+        float translation = linearSpeed;
+        translation *= Time.deltaTime;
+        transform.Translate(0, translation, 0);
+       
     }
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed;
 
+    void Update()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, 0, -rotation);
+    }
 
 }

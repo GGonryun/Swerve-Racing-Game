b0f3857cdbf19165d343400c395ff7edaf0bfe2e
Miguel Campos <amodestduck@gmail.com>
2018-11-24 23:12:55 -0800

    improving file structure and adding more sound effects

---
+++ Assets/Scripts/Attributes/PlayOnContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource), typeof(Rigidbody2D))]
+public class PlayOnContact : MonoBehaviour {
+
+    [SerializeField] private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        audio.Play();
+    }
+
+}
